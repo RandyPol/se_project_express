0603b4cabf1c17f331b8af3b332156dfaf56a3dd
Ran Pol <rnd809@gmail.com>
2023-03-13 01:55:46 -0400

    Import the routers into the entry file.

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose')
+const userRouter = require('./routes/users')
+const clothingItemRouter = require('./routes/clothingItems')
 
 const { PORT = 3001 } = process.env
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/weather_clothing_db')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/users', userRouter)
+app.use('/clothingItems', clothingItemRouter)
 
 app.listen(PORT)
