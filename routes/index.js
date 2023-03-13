ad5ffe72b11e5c120941dc0ae26fe3fbbdc0b697
Ran Pol <rnd809@gmail.com>
2023-03-13 12:05:04 -0400

    Create routes and controllers for likes.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router()
 // Import the routes
 const userRoutes = require('./users')
 const clothingItemRoutes = require('./clothingItems')
+const likeRoutes = require('./likes')
 
 // Implement the routes
 router.use('/users', userRoutes)
 router.use('/clothingItems', clothingItemRoutes)
+router.use('/items', likeRoutes)
 
 // Non-existent resources
 router.use((req, res) => {
