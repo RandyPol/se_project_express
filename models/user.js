4f3d1e7bcc092a77b7fbbf42b837618581de22e4
Ran Pol <rnd809@gmail.com>
2023-03-12 22:50:54 -0400

    Create the user schema with the property name and avatar. Use validator package.

---
+++ models/user.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Please enter a valid URL',
+    },
+  },
+})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
