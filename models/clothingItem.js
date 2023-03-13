4674703be2be8fe0e7be475a373c2b5e2e7f797b
Ran Pol <rnd809@gmail.com>
2023-03-12 23:33:50 -0400

    Create the clothingItem schema. Use validator package.

---
+++ models/clothingItem.js
@@ -0,0 +1,45 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const validator = require('validator')
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ['hot', 'warm', 'cold'],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'You must enter a valid URL',
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+})
+
+const ClothingItem = mongoose.model('clothingItem', clothingItemSchema)
+
+module.exports = ClothingItem
