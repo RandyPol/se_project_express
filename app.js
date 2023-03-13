1b27e7b913e651399c3e84ec9a3be03e7a9cbb13
Ran Pol <rnd809@gmail.com>
2023-03-12 22:29:12 -0400

    Create a database and install mongoose.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose')
 
 const { PORT = 3001 } = process.env
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/weather_clothing_db')
 
 app.listen(PORT)
