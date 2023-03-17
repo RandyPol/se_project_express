642d4b1993b73f8c74eaa9e0dbfcc04f52459b2e
Ran Pol <rnd809@gmail.com>
2023-03-17 12:22:51 -0400

    Expand the user schema: Email and Password. Give default values to Name and Avatar.

--- models/user.js
+++ models/user.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     minLength: 2,
     maxLength: 30,
+    default: 'Elise Bouer',
   },
   avatar: {
     type: String,
@@ -17,6 +18,22 @@ const userSchema = new mongoose.Schema({
       validator: (v) => validator.isURL(v),
       message: 'Please enter a valid URL',
     },
+    default:
+      'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png',
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Wrong email format',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 })
 
