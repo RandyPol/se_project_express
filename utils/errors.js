92fb7b75c25c8319b33ca7b82d0413499b250458
Ran Pol <rnd809@gmail.com>
2023-03-18 01:51:53 -0400

    Create a custom error classes.

--- utils/errors.js
+++ utils/errors.js
@@ -1,7 +1,19 @@
+// Custom error classes
+class AuthenticationError extends Error {
+  constructor(message = 'Incorrect password or email') {
+    super(message)
+    this.name = 'AuthenticationError'
+    this.statusCode = 401
+  }
+}
+
 module.exports = {
   BAD_REQUEST: 400,
   NOT_FOUND: 404,
   SERVER_ERROR: 500,
   DUPLICATE_ERROR: 11000,
   CONFLICT_DATA: 409,
+  UNAUTHORIZED_ERROR: 401,
 }
+
+module.exports.AuthenticationError = AuthenticationError
