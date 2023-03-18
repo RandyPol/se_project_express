26eebb58cc4782fe40b061371cf5457d9f8c336f
Ran Pol <rnd809@gmail.com>
2023-03-18 12:31:19 -0400

    Add the signup and signin routes and erase the users routes.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const { NOT_FOUND } = require('../utils/errors')
 // Import the routes
 const userRoutes = require('./users')
 const clothingItemRoutes = require('./clothingItems')
+const { createUser, login } = require('../controllers/users')
+
+// Sign up and sign in routes
+router.post('/signin', login)
+router.post('/signup', createUser)
 
 // Implement the routes
 router.use('/users', userRoutes)
