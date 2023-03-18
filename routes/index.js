0605f210642bc103180b99cbb87f2e0ef2f454a6
Ran Pol <rnd809@gmail.com>
2023-03-18 13:18:09 -0400

    Protect all routes except for: POSt/signin, Post/Signup/ Get/items.

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ const { NOT_FOUND } = require('../utils/errors')
 const userRoutes = require('./users')
 const clothingItemRoutes = require('./clothingItems')
 const { createUser, login } = require('../controllers/users')
+const { auth } = require('../middlewares/auth')
 
 // Sign up and sign in routes
 router.post('/signin', login)
 router.post('/signup', createUser)
 
 // Implement the routes
-router.use('/users', userRoutes)
+router.use('/users', auth, userRoutes)
 router.use('/items', clothingItemRoutes)
 
 // Non-existent resources
