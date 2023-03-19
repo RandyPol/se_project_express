76b0b2d967f89921788b2bea1fb520e1e2d9b23c
Ran Pol <rnd809@gmail.com>
2023-03-19 02:54:00 -0400

    Delete the error classes and update the error codes.

--- utils/errors.js
+++ utils/errors.js
@@ -1,21 +1,3 @@
-// Custom error classes
-// eslint-disable-next-line max-classes-per-file
-class AuthenticationError extends Error {
-  constructor(message = 'Incorrect password or email') {
-    super(message)
-    this.name = 'AuthenticationError'
-    this.statusCode = 401
-  }
-}
-
-class ForbiddenError extends Error {
-  constructor(message = 'Forbidden Action') {
-    super(message)
-    this.name = 'ForbiddenError'
-    this.statusCode = 401
-  }
-}
-
 module.exports = {
   BAD_REQUEST: 400,
   NOT_FOUND: 404,
@@ -23,7 +5,5 @@ module.exports = {
   DUPLICATE_ERROR: 11000,
   CONFLICT_DATA: 409,
   UNAUTHORIZED_ERROR: 401,
+  FORBIDDEN_ERROR: 403,
 }
-
-module.exports.AuthenticationError = AuthenticationError
-module.exports.AuthenticationError = ForbiddenError
