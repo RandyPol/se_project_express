f4c270e68e166e3c71775bf9f454cedbc82b42c3
Ran Pol <rnd809@gmail.com>
2023-04-22 14:47:02 -0400

    Install celebrate, Joi, and validator. Add validateURL function.

---
+++ middlewares/validation.js
@@ -0,0 +1,10 @@
+const { Joi, celebrate } = require('celebrate')
+const validator = require('validator')
+
+
+const validateURL = (value, helpers) => {
+    if (validator.isURL(value)) {
+      return value;
+    }
+    return helpers.error('string.uri');
+  };
