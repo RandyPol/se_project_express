c6455a28f3baeef20f7b4dadcbf185d127fbdd1d
Ran Pol <rnd809@gmail.com>
2023-04-22 16:16:15 -0400

    Add validation for when a user is created.

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -26,4 +26,29 @@ module.exports.validateCardBody = celebrate({
       'string.uri': 'The "imageUrl" field must be a valid URL',
     }),
   }),
-});
+})
+
+// The user info body when a user is created
+
+module.exports.validateUserInfoBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      'string.min': 'The minimum length of the "name" field is 2',
+      'string.max': 'The maximum length of the "name" field is 30',
+      'string.empty': 'The "name" field must be filled in',
+    }),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      'string.empty': 'The "avatar" field must be filled in',
+      'string.uri': 'The "avatar" field must be a valid URL',
+    }),
+    email: Joi.string().required().email().messages({
+      'string.empty': 'The "email" field must be filled in',
+      'string.email': 'The "email" field must be a valid email',
+    }),
+    password: Joi.string().required().min(9).messages({
+      'string.min': 'The minimum length of the "password" field is 9',
+      'string.empty': 'The "password" field must be filled in',
+    }),
+  }),
+})
+
