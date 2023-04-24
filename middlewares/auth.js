05fea2fdb3e21cfb749054d722c37a58c99b8d00
Ran Pol <rnd809@gmail.com>
2023-04-24 15:19:20 -0400

    Throw the corresponding Unauthorize error.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,25 @@
 const jwt = require('jsonwebtoken')
-const errorCode = require('../utils/errorsCode')
-
 const { JWT_SECRET } = require('../utils/config')
-
-const handleAuthError = (res) => {
-  res
-    .status(errorCode.UNAUTHORIZED_ERROR)
-    .send({ message: 'Authorization Error' })
-}
+const UnauthorizedError = require('../utils/errors/UnauthorizedError')
 
 const extractBearerToken = (header) => header.replace('Bearer ', '')
 
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers
+
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res)
+    throw new UnauthorizedError('Authorization Error')
   }
+
   const token = extractBearerToken(authorization)
   let payload
+
   try {
     payload = jwt.verify(token, JWT_SECRET)
   } catch (err) {
-    return handleAuthError(res)
+    throw new UnauthorizedError('Authorization Error')
   }
+
   req.user = payload
   return next()
 }
