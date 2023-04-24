1fea8ae6a4c2f07b6945b5141cfc51ab4fac2dd9
Ran Pol <rnd809@gmail.com>
2023-04-24 16:38:51 -0400

    Store rate limiter configuration in a separate file and import it into app.js.

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
-const rateLimit = require('express-rate-limit')
 const cors = require('cors')
 const helmet = require('helmet')
 const { errors } = require('celebrate')
 const routes = require('./routes')
 const errorHandler = require('./utils/errorHandler')
 const { requestLogger, errorLogger } = require('./middlewares/logger')
+const limiter = require('./middlewares/limiter')
 
 const { PORT = 3001 } = process.env
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/weather_clothing_db')
-
-// Seting up the rate limiting middleware
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 100, // limit each IP to 100 requests per windowMs
-})
 
 app.use(helmet())
 app.use(limiter)
