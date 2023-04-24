0abac52abe01ec9f00830e75223c1b38062a792e
Ran Pol <rnd809@gmail.com>
2023-04-24 16:12:46 -0400

    Add the validateUserUpdateBody with Joi.

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -53,6 +53,22 @@ module.exports.validateUserInfoBody = celebrate({
   }),
 })
 
+// The user info body when a user is updated
+
+module.exports.validateUserUpdateBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      'string.min': 'The minimum length of the "name" field is 2',
+      'string.max': 'The maximum length of the "name" field is 30',
+      'string.empty': 'The "name" field must be filled in',
+    }),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      'string.empty': 'The "avatar" field must be filled in',
+      'string.uri': 'The "avatar" field must be a valid URL',
+    }),
+  }),
+})
+
 // Authentication when a user logs in
 
 module.exports.validateLoginBody = celebrate({
