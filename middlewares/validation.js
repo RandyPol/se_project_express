2418222776c2dde8fa1371fe776d841dff52b3f6
Ran Pol <rnd809@gmail.com>
2023-04-24 15:22:47 -0400

    Add the weather field to the Joi object.

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -25,6 +25,7 @@ module.exports.validateCardBody = celebrate({
       'string.empty': 'The "imageUrl" field must be filled in',
       'string.uri': 'The "imageUrl" field must be a valid URL',
     }),
+    weather: Joi.string().valid('hot', 'warm', 'cold'),
   }),
 })
 
