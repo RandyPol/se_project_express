6f349581110f1c9823c9c82d96da03517f20faec
Ran Pol <rnd809@gmail.com>
2023-04-24 16:14:52 -0400

    Add the celebrate schema validateUserUpdateBody.

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const { getCurrentUser, updateUser } = require('../controllers/users')
 
+const { validateUserUpdateBody } = require('../middlewares/validation')
+
 router.get('/me', getCurrentUser)
-router.patch('/me', updateUser)
+router.patch('/me', validateUserUpdateBody, updateUser)
 
 module.exports = router
